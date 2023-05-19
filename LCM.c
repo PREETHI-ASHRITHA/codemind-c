69e65456de3ccd921407139e89164a2e5d2e6046
G PREETHI ASHRITHA <119730094+PREETHI-ASHRITHA@users.noreply.github.com>
2023-05-19 21:25:43 +0530

    #include<stdio.h> int main() {     int a,b,lcm;     scanf("%d%d",&a,&b);     lcm=(a>b)?a:b;     while(1)     {         if(lcm%a==0 &&lcm%b==0)         {             printf("%d",lcm);             break;         }         lcm++;     }
    }

--- LCM.c
+++ LCM.c
@@ -6,11 +6,12 @@ int main()
     lcm=(a>b)?a:b;
     while(1)
     {
-        if(lcm%a==0&&lcm%b==0)
+        if(lcm%a==0 &&lcm%b==0) 
         {
             printf("%d",lcm);
             break;
         }
         lcm++;
     }
+    
 }
