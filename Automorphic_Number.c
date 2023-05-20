213a70aaec1153965329034556808ade446f5390
G PREETHI ASHRITHA <119730094+PREETHI-ASHRITHA@users.noreply.github.com>
2023-05-20 11:05:11 +0530

    #include<stdio.h> int main() {     int n,num,sqr;     int temp,rem,f=0;     scanf("%ld",&n);     num=n;     sqr=n*n;     temp=10;     while(n!=0)     {         rem=sqr%temp;         if(num==rem)         {             f=1;             break;         }         n=n/10;         temp=temp*10;     }     if(f==1)     {         printf("Automorphic Number");     }     else     {         printf("Not an Automorphic Number");     } }

--- Automorphic_Number.c
+++ Automorphic_Number.c
@@ -1,16 +1,24 @@
 #include<stdio.h>
 int main()
 {
-    int t,s,c=1,n;
-    scanf("%d",&n);
-    t=n;
-    s=n*n;
+    int n,num,sqr;
+    int temp,rem,f=0;
+    scanf("%ld",&n);
+    num=n;
+    sqr=n*n;
+    temp=10;
     while(n!=0)
     {
-        c=c*10;
+        rem=sqr%temp;
+        if(num==rem)
+        {
+            f=1;
+            break;
+        }
         n=n/10;
+        temp=temp*10;
     }
-    if(s%c==t)
+    if(f==1)
     {
         printf("Automorphic Number");
     }
